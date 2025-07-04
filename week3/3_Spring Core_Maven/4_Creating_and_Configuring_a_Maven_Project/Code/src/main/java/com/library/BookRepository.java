191a6d41077abe85f7770e872bf9541c8b98d271
nidhi <nidhisbkp@gmail.com>
2025-07-04 10:01:28 +0530

    added 3rd answer of spring core maven

---
+++ week3/3_Spring Core_Maven/4_Creating_and_Configuring_a_Maven_Project/Code/src/main/java/com/library/BookRepository.java
@@ -0,0 +1,9 @@
+package com.library;
+
+public class BookRepository {
+		// TODO Auto-generated constructor stub
+		 public void saveBook(String title) {
+		        System.out.println("Saving book: " + title);
+		    }
+	}
+
